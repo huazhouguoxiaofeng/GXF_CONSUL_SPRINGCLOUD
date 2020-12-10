7647f056e6e633d8d6868bee674e3d409f2df482
huazhouguoxiaofeng <1121553503@qq.com>
2020-12-11 00:30:03 +0800

    上传了一个md文件

--- strive_user/src/main/java/com/gxf/user/back/handler/JsonBackHandler.java
+++ strive_user/src/main/java/com/gxf/user/back/handler/JsonBackHandler.java
@@ -11,31 +11,31 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-@RestControllerAdvice
-public class JsonBackHandler implements ResponseBodyAdvice<Object> {
+//@RestControllerAdvice
+//public class JsonBackHandler implements ResponseBodyAdvice<Object> {
 
     /**
      * 判断是否执行 beforeBodyWrite 方法，true 为执行，false 为不执行
      */
-    @Override
-    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
-        System.out.println("returnType::::" + returnType);
-        return MappingJackson2HttpMessageConverter.class.isAssignableFrom(converterType);
-//        return true;
-    }
+//    @Override
+//    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+//        System.out.println("returnType::::" + returnType);
+//        return MappingJackson2HttpMessageConverter.class.isAssignableFrom(converterType);
+////        return true;
+//    }
 
-    @Override
-    public Object beforeBodyWrite(Object o,
-                                  MethodParameter methodParameter,
-                                  MediaType mediaType,
-                                  Class aClass,
-                                  ServerHttpRequest serverHttpRequest,
-                                  ServerHttpResponse serverHttpResponse) {
-        System.out.println("caocaocao");
-        MsgResponse msgResponse = new MsgResponse();
-        msgResponse.setCode(HttpStatus.OK.value());
-        msgResponse.setMsg(HttpStatus.OK.name());
-        msgResponse.setData(o);
-        return msgResponse;
-    }
-}
+//    @Override
+//    public Object beforeBodyWrite(Object o,
+//                                  MethodParameter methodParameter,
+//                                  MediaType mediaType,
+//                                  Class aClass,
+//                                  ServerHttpRequest serverHttpRequest,
+//                                  ServerHttpResponse serverHttpResponse) {
+//        System.out.println("caocaocao");
+//        MsgResponse msgResponse = new MsgResponse();
+//        msgResponse.setCode(HttpStatus.OK.value());
+//        msgResponse.setMsg(HttpStatus.OK.name());
+//        msgResponse.setData(o);
+//        return msgResponse;
+//    }
+//}
